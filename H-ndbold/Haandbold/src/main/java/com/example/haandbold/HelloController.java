1c4a5cce94619faec3c0baaf9cb7930c7a4e89fb
MadsKaiser <madka01@eamv.dk>
2025-01-12 10:49:07 +0100

    Add files via upload

---
+++ H-ndbold/Haandbold/src/main/java/com/example/haandbold/HelloController.java
@@ -0,0 +1,61 @@
+package com.example.haandbold;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class HelloController {
+
+    @FXML
+    private Label HoldEt;
+
+    @FXML
+    private Label HoldTo;
+
+    @FXML
+    private Label KampTid;
+
+    @FXML
+    private ListView<?> KampUdskrift;
+
+    @FXML
+    private Label MålEt;
+
+    @FXML
+    private Button MålKnapEt;
+
+    @FXML
+    private Button MålKnapTo;
+
+    @FXML
+    private Label MålTo;
+
+    @FXML
+    private Button UdvisningKnapEt;
+
+    @FXML
+    private Button UdvisningknapTo;
+
+    @FXML
+    void OnBanClickEt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnBanClickTo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onGoalClickLeft(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onGoalClickRight(ActionEvent event) {
+
+    }
+
+}
