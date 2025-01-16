200acf004746c77ab7346f6f2b5be232a1abf6b9
MadsKaiser <madka01@eamv.dk>
2025-01-16 09:35:47 +0100

    Add files via upload

---
+++ H-ndbold/Handbold/src/main/java/com/example/handbold/Main.java
@@ -0,0 +1,24 @@
+package com.example.handbold;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        primaryStage.setTitle("Håndbold Kamp");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ H-ndbold/Handbold/src/main/java/com/example/handbold/SecondViewController.java
@@ -0,0 +1,13 @@
+package com.example.handbold;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class SecondViewController {
+
+    @FXML
+    private ListView<?> LigaStillingList;
+
+
+
+}
