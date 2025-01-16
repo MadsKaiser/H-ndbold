4a4a285c8aa6ca03ee9e624a7e7ff1b9d30a5503
Benjamin Bjertrup <iggythefrogster@gmail.com>
2025-01-16 12:24:53 +0100

    Add files via upload

---
+++ H-ndbold-main/H-ndbold-main/H-ndbold/Handbold/src/main/java/com/example/handbold/LigaStillingController.java
@@ -0,0 +1,21 @@
+package com.example.handbold;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class LigaStillingController {
+
+    @FXML
+    private TableColumn<?, ?> Holdnavn;
+
+    @FXML
+    private TableView<?> Liga;
+
+    @FXML
+    private TableColumn<?, ?> Placering;
+
+    @FXML
+    private TableColumn<?, ?> Point;
+
+}
