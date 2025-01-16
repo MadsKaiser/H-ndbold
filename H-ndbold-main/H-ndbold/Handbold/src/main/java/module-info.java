b6819dffeb1bff4665a712a95164f0661b4a83c1
MadsKaiser <madka01@eamv.dk>
2025-01-16 22:05:00 +0100

    Delete H-ndbold-main directory

--- H-ndbold-main/H-ndbold/Handbold/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.handbold {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.example.handbold to javafx.fxml;
-    exports com.example.handbold;
-}
